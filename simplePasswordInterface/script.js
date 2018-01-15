40ee46933a015e100b571e6339d0e7435b7af463
PawelKurpinski <pkurpinski@wp.pl>
2018-01-15 23:06:00 +0100

    15.01

---
+++ simplePasswordInterface/script.js
@@ -0,0 +1,12 @@
+
+function checkPassword(){
+    var val = document.getElementById('haslo').value;
+    var count = val.length;
+
+    if (value == "" || value == 0) {
+        document.getElementById('wynik').innerHTML = "Ogarnij się! wpisz coś coś sensownego!"
+    }
+    else if {
+        document.getElementById('wynik').innerHTML;
+    }
+}

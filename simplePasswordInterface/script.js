102bafa519106869280c052d465484459f8231bf
PawelKurpinski <pkurpinski@wp.pl>
2018-01-16 22:49:10 +0100

    16.01

--- simplePasswordInterface/script.js
+++ simplePasswordInterface/script.js
@@ -1,13 +1,15 @@
 
 function checkPassword(){
     var val = document.getElementById('haslo').value;
-    var count = haslo.length;
-    alert(count);
+    var password = val.length;
 
     if (val == "" || val == 0) {
         document.getElementById('wynik').innerHTML = "Ogarnij się! wpisz coś coś sensownego!"
     }
-    else if (count >= 7) {
+    else if (password >= 7 && check(password) == true) {
+        document.getElementById('wynik').innerHTML = '<span style="color:red;">Hasło jest dobre!</span>';
+    }
+    else if ( 4 <= count <= 7 && ) {
         document.getElementById('wynik').innerHTML;
     }
 }

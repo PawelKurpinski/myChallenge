6f2cfed2edfc9ee0b4b8c804987d521fed60b78c
PawelKurpinski <pkurpinski@wp.pl>
2018-01-18 23:04:13 +0100

    18.01

--- simplePasswordInterface/script.js
+++ simplePasswordInterface/script.js
@@ -1,5 +1,6 @@
 function check(x) {
     var d = x.length;
+
     for (i = 0; i < d; i++) {
         if (x.charAt(i) == "0" || x.charAt(i) == "1" || x.charAt(i) == "2" || x.charAt(i) == "3" || x.charAt(i) == "4" || x.charAt(i) == "5" || x.charAt(i) == "6" || x.charAt(i) == "7" || x.charAt(i) == "8" || x.charAt(i) == "9")
         return true;
@@ -9,14 +10,13 @@ function check(x) {
 
 function checkPassword() {
     var val = document.getElementById('haslo').value;
-    var password = val.length;
+    var pass = val.length;
 
-    if (val == "" || val == 0)
-        document.getElementById('wynik').innerHTML = "Ogarnij się! wpisz coś coś sensownego!";
-    else if (password >= 7 && check(password) == true)
+    if (val == "")
+        document.getElementById('wynik').innerHTML = "Ogarnij się! wpisz coś!";
+    else if (pass >= 7 && check(val) == true)
         document.getElementById('wynik').innerHTML = '<span style="color:green;">Hasło jest dobre!</span>';
-    else if (password >= 4 && password <= 6 && check(password) == true)
-        document.getElementById('wynik').innerHTML = '<span style="color:blu;">Hasło jest średie! Żyjmy coś!</span>';
-    else (document.getElementById('wynik').innerHTML = '<span style="color:blou;">Hasło jest lipne! Pobudka!!</span>';
-
+    else if (pass >= 4 && pass <= 6 && check(val) == true)
+        document.getElementById('wynik').innerHTML = '<span style="color:orange;">Hasło jest średie! Żyjmy coś!</span>';
+    else document.getElementById('wynik').innerHTML = '<span style="color:blue;">Hasło jest za słabe! Pobudka!!</span>';
 }
